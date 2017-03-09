3f7b6acb997139ce8ab645ce8efb02eb3fe93dbb
Dino Soru <dino.soru@gmail.com>
2017-03-09 18:07:06 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var rp = require('request-promise');
+
+router.get('/', function(req, res, next) {
+  res.render('search');
+});
+
+
+/* TODO Search in POST perche' non cacheabile? */
+router.get('/search', function(req, res, next) {
+  let text = req.query.q;
+  let options = {
+    method:'GET',
+    uri:'http://localhost:3002/contents?text=' + text,
+    json:true
+  }
+  rp(options)
+  .then((results) => {
+    res.json(results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
+
+module.exports = router;
+
+/*
+config file per host ricerche
+
+middleware auth
+
+- GET visualizza la pagina di ricerca
+  - pagina chiama POST che effettua la ricerca
+    - POST forwarda al microservizio contentms
+    - POST restituisce risultati a pagina di ricerca
+  - pagina di ricerca aggiorna i contenuti dinamicamente
+
+*/
