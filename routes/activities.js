330b5f466d02c4863c43623de315a2de3dbdc13f
dino <dino.soru@gmail.com>
2017-11-22 16:43:23 +0100

    refactoring routes

--- routes/activities.js
+++ routes/activities.js
@@ -2,6 +2,8 @@ var request = require('request');
 var rp = require('request-promise');
 var config = require('propertiesmanager').conf;
 var common = require('./render');
+var express = require('express');
+var router = express.Router();
 
 let baseUrl = config.contentUIUrl + (config.contentUIUrl.endsWith('/') ? '' : '/');
 let contentUrl = config.contentUrl + (config.contentUrl.endsWith('/') ? '' : '/');
@@ -9,72 +11,33 @@ config.contentUrl = contentUrl;
 let uploadUrl = config.uploadUrl + (config.uploadUrl.endsWith('/') ? '' : '/');
 let _userUrl = config.userUrl + (config.userUrl.endsWith('/') ? '' : '/');
 
-module.exports = {
-	//view activity
-	get: (req, res, next) => {
-		getActivity(req, res);
-	},
-	
-	//edit activity
-	put: (req, res, next) => {
-		getActivity(req, res, true);
-	},
 
-	//new activity (blank form)
-	post: (req, res, next) => {
-		let access_token = req.query.access_token || null; //SOSTITUIRE CON req.headers.authorization.split(' ')[1] || null;		
-		return common.renderPage(res, 'activities/form_activity', {
-			activityBody: undefined,
-			params: JSON.stringify(req.params),
-			query: JSON.stringify(req.query),
-			baseUrl:baseUrl,
-			uploadUrl:uploadUrl,
-			contentUrl:contentUrl,
-			access_token: access_token,
-			contentAdminTypes: config.contentAdminTokenType
-		});
-	},
+//ask userms for admins details
+router.get('/admins',	 (req, res, next) => {
+	let admins = req.query.adm;
 
-	//ask userms for admins details
-	admins: (req, res, next) => {
-		let admins = req.query.adm;
-
-		getAdmins(admins)
-		.then(users => {
-			res.json(users)
-		})
-		.catch(e => {
-			console.log(e);
-			res.boom.badImplementation();
-		})
-	}
-}
-
-
-function getActivity(req, res, edit) {
-	let activity_id = req.params.id;
-	let abody = undefined;
-	let access_token = req.query.access_token || null; //SOSTITUIRE CON req.headers.authorization.split(' ')[1] || null;
-	rp(contentUrl+"contents/"+activity_id)
-	.then(body => {
-		abody = JSON.parse(body);		
-		common.renderPage(res, 'activities/' + (edit ? 'form_activity' : 'view_activity'), {
-			activityBody: abody,
-			activityId: activity_id,
-			params: edit ? JSON.stringify(req.params) : undefined,
-			query: edit ? JSON.stringify(req.query) : undefined,
-			baseUrl: baseUrl,
-			uploadUrl: uploadUrl,
-			contentUrl: contentUrl,
-			access_token: access_token,
-			contentAdminTypes: config.contentAdminTokenType
-		});
+	getAdmins(admins)
+	.then(users => {
+		res.json(users)
 	})
 	.catch(e => {
 		console.log(e);
 		res.boom.badImplementation();
 	})
-} 
+});
+
+
+//search for users by mail
+router.get('/users', (req, res, next) => {res.json([])}); //TODO chiedere a ale di convertire da action in search con query string ?q=
+router.get('/users/:q',		 (req, res, next) => {	
+	getUsersByMail(req.params.q)
+	.then(users => {res.json(users)})
+	.catch(e => {
+		console.log(e);
+		res.boom.badImplementation();
+	})
+});
+
 
 
 function getAdmins(admIds) {
@@ -103,3 +66,26 @@ function getAdmins(admIds) {
 	}
 	else return [];
 }
+
+
+function getUsersByMail(mail) {	
+	return new Promise((resolve, reject) => {
+		rp({
+			uri:_userUrl + 'users/actions/email/find/' + mail,
+			method: 'GET',
+			json:true,
+			headers: {
+				authorization: "Bearer " + config.auth_token
+			}
+		})
+		.then(users => {
+			resolve(users.data)
+		})
+		.catch(e => {
+			console.log(e);
+			reject(e);
+		})
+	});
+}
+
+module.exports = router;
